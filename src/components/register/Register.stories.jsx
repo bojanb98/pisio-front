960889fabf6d522aa33baa3ad737f9d6c1f48ca0
Bojan Bulatovic <bojan.bulatovic03@gmail.com>
2021-07-01 15:47:19 +0200

    Completed Register page layout, fixed Login page

---
+++ src/components/register/Register.stories.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import Register from './Register';
+
+export default {
+  component: Register,
+  title: 'Pages/Register',
+}
+
+export const RegisterPage = () => <Register />
